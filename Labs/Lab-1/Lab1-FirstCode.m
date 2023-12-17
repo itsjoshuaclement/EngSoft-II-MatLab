9d15fb724d9b5dfa333ab7d78efb6b066bb6eaac
ItsJoshua <116568341+itsjoshuaclement@users.noreply.github.com>
2023-12-16 22:48:22 -0500

    Update Lab1-FirstCode.m

--- Labs/Lab-1/Lab1-FirstCode.m
+++ Labs/Lab-1/Lab1-FirstCode.m
@@ -1,5 +1,7 @@
-clear
-clc
+clear all;   
+clc;          
+close all;    
+format short;
 
 name = input('What is your name?  ','s');
 fprintf('Congratulations %s, you have executed your first MATLAB code!\n'...
